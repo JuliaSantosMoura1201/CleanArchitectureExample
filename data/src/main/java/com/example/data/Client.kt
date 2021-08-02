43da959a9e19643db562061ab3d96c2640fec32a
Júlia Santos de Moura <julia.moura@bancointer.com.br>
2021-08-02 09:04:20 -0300

    service

---
+++ data/src/main/java/com/example/data/Client.kt
@@ -0,0 +1,30 @@
+package com.example.data
+
+import java.util.concurrent.TimeUnit
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Client{
+
+    fun provideHttpClient() =
+        OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .apply{
+                val logging = HttpLoggingInterceptor()
+                logging.level = HttpLoggingInterceptor.Level.BODY
+                addInterceptor(logging)
+            }.build()
+
+    inline fun <reified T> createWebService(okHttpClient: OkHttpClient): T =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .build()
+            .create(T::class.java)
+
+    const val  BASE_URL = "https://ronreiter-meme-geneator.p.rapidapi.com/meme"
+}
