e4ca884541a342b8889611c81165d4f8650aedfb
Júlia Santos de Moura <julia.moura@bancointer.com.br>
2021-08-02 13:32:36 -0300

    implement ( Either.kt ) basic domain

---
+++ domain/src/main/java/com/example/domain/Either.kt
@@ -0,0 +1,11 @@
+package com.example.domain.di.repositories
+
+sealed class Either<out A, out B>
+
+    data class Success<out A>(
+        val data: A
+    ): Either<A, Nothing>()
+
+    data class Failure<out B>(
+        val cause: B
+    ): Either<Nothing, B>()
